3e2265cceb234c73526015366c1aae0b3ef13995
corentin <corentin@ltbl.fr>
2024-08-23 16:31:47 +0200

    Modify CefString conversion return type

--- src/settings.rs
+++ src/settings.rs
@@ -8,79 +8,41 @@ use crate::{string::CefString, LogItems, LogSeverity};
 #[derive(Debug, Default, Clone)]
 pub struct Settings {
     pub no_sandbox: bool,
-
     pub browser_subprocess_path: CefString,
-
     pub framework_dir_path: CefString,
-
     pub main_bundle_path: CefString,
-
     pub chrome_runtime: bool,
-
     pub multi_threaded_message_loop: bool,
-
     pub external_message_pump: bool,
-
     pub windowless_rendering_enabled: bool,
-
     pub command_line_args_disabled: bool,
-
     pub cache_path: CefString,
-
     pub root_cache_path: CefString,
-
-    pub user_data_path: CefString,
-
     pub persist_session_cookies: bool,
-
     pub persist_user_preferences: bool,
-
     pub user_agent: CefString,
-
     pub user_agent_product: CefString,
-
     pub locale: CefString,
-
     pub log_file: CefString,
-
     pub log_severity: LogSeverity,
-
     pub log_items: LogItems,
-
     pub javascript_flags: CefString,
-
     pub resources_dir_path: CefString,
-
     pub locales_dir_path: CefString,
-
     pub pack_loading_disabled: bool,
-
     pub remote_debugging_port: u32,
-
     pub uncaught_exception_stack_size: u32,
-
     pub background_color: u32,
-
     pub accept_language_list: CefString,
-
     pub cookieable_schemes_list: CefString,
-
     pub cookieable_schemes_exclude_defaults: bool,
-
     pub chrome_policy_id: CefString,
-
     pub chrome_app_icon_id: i32,
 }
 
 impl Settings {
-    pub fn new() -> Self {
-        Self {
-            // no_sandbox: true,
-            remote_debugging_port: 5566,
-            // log_severity: LogSeverity::LOGSEVERITY_VERBOSE,
-            // log_items: LogItems::LOG_ITEMS_DEFAULT,
-            ..Default::default()
-        }
+    pub(crate) fn from_mut_ptr(raw: *mut cef_settings_t) -> Self {
+        Self::from(unsafe { &*raw })
     }
 
     pub fn get_raw(&self) -> cef_settings_t {
@@ -117,6 +79,44 @@ impl Settings {
             cookieable_schemes_exclude_defaults: self.cookieable_schemes_exclude_defaults as c_int,
             chrome_policy_id: self.chrome_policy_id.get_raw(),
             chrome_app_icon_id: self.chrome_app_icon_id,
+        }
+    }
+}
+
+impl From<&cef_settings_t> for Settings {
+    fn from(raw: &cef_settings_t) -> Self {
+        Settings {
+            no_sandbox: raw.no_sandbox > 0,
+            browser_subprocess_path: raw.browser_subprocess_path.try_into().expect("Error converting browser_subprocess_path string"),
+            framework_dir_path: raw.framework_dir_path.try_into().expect("Error converting framework_dir_path string"),
+            main_bundle_path: raw.main_bundle_path.try_into().expect("Error converting main_bundle_path string"),
+            chrome_runtime: raw.chrome_runtime > 0,
+            multi_threaded_message_loop: raw.multi_threaded_message_loop > 0,
+            external_message_pump: raw.external_message_pump > 0,
+            windowless_rendering_enabled: raw.windowless_rendering_enabled > 0,
+            command_line_args_disabled: raw.command_line_args_disabled > 0,
+            cache_path: raw.cache_path.try_into().expect("Error converting cache_path string"),
+            root_cache_path: raw.root_cache_path.try_into().expect("Error converting root_cache_path string"),
+            persist_session_cookies: raw.persist_session_cookies > 0,
+            persist_user_preferences: raw.persist_user_preferences > 0,
+            user_agent: raw.user_agent.try_into().expect("Error converting user_agent string"),
+            user_agent_product: raw.user_agent_product.try_into().expect("Error converting user_agent_product string"),
+            locale: raw.locale.try_into().expect("Error converting locale string"),
+            log_file: raw.log_file.try_into().expect("Error converting log_file string"),
+            log_severity: raw.log_severity,
+            log_items: raw.log_items,
+            javascript_flags: raw.javascript_flags.try_into().expect("Error converting javascript_flags string"),
+            resources_dir_path: raw.resources_dir_path.try_into().expect("Error converting resources_dir_path string"),
+            locales_dir_path: raw.locales_dir_path.try_into().expect("Error converting locales_dir_path string"),
+            pack_loading_disabled: raw.pack_loading_disabled > 0,
+            remote_debugging_port: raw.remote_debugging_port as u32,
+            uncaught_exception_stack_size: raw.uncaught_exception_stack_size as u32,
+            background_color: raw.background_color as cef_color_t,
+            accept_language_list: raw.accept_language_list.try_into().expect("Error converting accept_language_list string"),
+            cookieable_schemes_list: raw.cookieable_schemes_list.try_into().expect("Error converting cookieable_schemes_list string"),
+            cookieable_schemes_exclude_defaults: raw.cookieable_schemes_exclude_defaults > 0,
+            chrome_policy_id: raw.chrome_policy_id.try_into().expect("Error converting chrome_policy_id string"),
+            chrome_app_icon_id: raw.chrome_app_icon_id,
         }
     }
 }
--- src/string.rs
+++ src/string.rs
@@ -11,10 +11,17 @@
 use cef_sys::{
     cef_string_list_t, cef_string_map_t, cef_string_userfree_utf16_t, cef_string_utf16_t,
 };
+use widestring::error::ContainsNul;
 use std::collections::HashMap;
 use std::fmt::Display;
 use std::ptr::null_mut;
 use widestring::U16CString;
+
+#[derive(Debug)]
+pub enum CefStringError {
+    NullPointer,
+    Conversion
+}
 
 /// Helper type to deal with Cef string. It's essentially an UTF-16 C string.
 #[derive(Debug, Default, Clone)]
@@ -27,22 +34,21 @@ impl CefString {
 
     /// Create a `CefString` from raw `cef_string_utf16_t` pointer. If the pointer is null or it fails
     /// to convert to `U16CString`, this method will returns `None`.
-    pub unsafe fn from_raw(ptr: *const cef_string_utf16_t) -> Option<CefString> {
+    pub unsafe fn from_raw(ptr: *const cef_string_utf16_t) -> Result<CefString, CefStringError> {
         if ptr.is_null() {
-            None
+            Err(CefStringError::NullPointer)
         } else {
             // It's a smart pointer, so cef retains ownership and will call the dtor
             unsafe {
                 U16CString::from_ptr((*ptr).str_, (*ptr).length)
-                    .ok()
-                    .map(CefString)
+                    .map(CefString).map_err(|_| CefStringError::Conversion)
             }
         }
     }
 
     /// Create a `CefString` from raw `cef_string_userfree_utf16_t` pointer. If the pointer is null or it fails
     /// to convert to `U16CString`, this method will returns `None`.
-    pub unsafe fn from_userfree_cef(ptr: cef_string_userfree_utf16_t) -> Option<CefString> {
+    pub unsafe fn from_userfree_cef(ptr: cef_string_userfree_utf16_t) -> Result<CefString, CefStringError> {
         let res = unsafe { Self::from_raw(ptr) };
         unsafe {
             cef_sys::cef_string_userfree_utf16_free(ptr);
@@ -61,6 +67,14 @@ impl CefString {
     }
 }
 
+impl TryFrom<cef_string_utf16_t> for CefString {
+    type Error = ContainsNul<u16>;
+
+    fn try_from(value: cef_string_utf16_t) -> Result<Self, Self::Error> {
+        unsafe { U16CString::from_ptr(value.str_, value.length).map(CefString) }
+    }
+}
+
 impl Display for CefString {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         self.0.display().fmt(f)
@@ -73,7 +87,7 @@ pub unsafe fn parse_string_list(ptr: cef_string_list_t) -> Vec<String> {
     for i in 0..count {
         let value = null_mut();
         if cef_sys::cef_string_list_value(ptr, i, value) > 0 {
-            if let Some(v) = CefString::from_raw(value) { res.push(v.to_string()) }
+            if let Ok(v) = CefString::from_raw(value) { res.push(v.to_string()) }
         }
     }
     res
@@ -88,7 +102,7 @@ pub unsafe fn parse_string_map(ptr: cef_string_map_t) -> HashMap<String, String>
         cef_sys::cef_string_map_key(ptr, i, key);
         cef_sys::cef_string_map_value(ptr, i, value);
 
-        CefString::from_raw(key)
+        let _ = CefString::from_raw(key)
             .map(|k| CefString::from_raw(value).map(|v| res.insert(k.to_string(), v.to_string())));
     }
     res
