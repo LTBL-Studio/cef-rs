4ffcddf1d68f68023b3aee25fc0e2fbeb06dfff6
corentin <corentin@ltbl.fr>
2024-08-23 16:33:58 +0200

    Change CefRect, CefAcceleratedPaintInfo and CefScreenInfo fields types

--- src/render_utils.rs
+++ src/render_utils.rs
@@ -9,8 +9,8 @@ pub type CefAcceleratedPaintNativePixmapPlaneInfo = _cef_accelerated_paint_nativ
 pub struct CefRect {
     pub x: i32,
     pub y: i32,
-    pub width: i32,
-    pub height: i32,
+    pub width: u32,
+    pub height: u32,
 }
 
 impl CefRect {
@@ -21,12 +21,13 @@ impl CefRect {
     pub(crate) fn from_ptr(raw: *const cef_rect_t) -> Self {
         Self::from(unsafe { &*raw })
     }
+
     pub(crate) fn from(raw: &cef_rect_t) -> Self {
         CefRect {
             x: raw.x,
             y: raw.y,
-            width: raw.width,
-            height: raw.height,
+            width: raw.width as u32,
+            height: raw.height as u32,
         }
     }
 
@@ -34,14 +35,25 @@ impl CefRect {
         cef_rect_t {
             x: self.x,
             y: self.y,
-            width: self.width,
-            height: self.height,
+            width: self.width as i32,
+            height: self.height as i32,
         }
     }
 
     pub(crate) fn from_array(count: usize, rects: *const cef_rect_t) -> Vec<CefRect> {
         let raw_rects = unsafe { core::slice::from_raw_parts(rects, count) };
         raw_rects.iter().map(Self::from).collect()
+    }
+}
+
+impl From<cef_rect_t> for CefRect {
+    fn from(raw: cef_rect_t) -> Self {
+        CefRect {
+            x: raw.x,
+            y: raw.y,
+            width: raw.width as u32,
+            height: raw.height as u32,
+        }
     }
 }
 
@@ -100,7 +112,7 @@ impl CefRange {
 #[derive(Debug, Clone)]
 pub struct CefAcceleratedPaintInfo {
     pub planes: [CefAcceleratedPaintNativePixmapPlaneInfo; 4usize],
-    pub plane_count: i32,
+    pub plane_count: u32,
     pub modifier: u64,
     pub format: ColorType,
 }
@@ -112,7 +124,7 @@ impl CefAcceleratedPaintInfo {
     pub(crate) fn from(raw: &_cef_accelerated_paint_info_t) -> Self {
         CefAcceleratedPaintInfo {
             planes: raw.planes,
-            plane_count: raw.plane_count,
+            plane_count: raw.plane_count as u32,
             modifier: raw.modifier,
             format: raw.format,
         }
@@ -121,7 +133,7 @@ impl CefAcceleratedPaintInfo {
     pub fn into_raw(self) -> _cef_accelerated_paint_info_t {
         _cef_accelerated_paint_info_t {
             planes: self.planes,
-            plane_count: self.plane_count,
+            plane_count: self.plane_count as i32,
             modifier: self.modifier,
             format: self.format,
         }
@@ -132,25 +144,14 @@ impl CefAcceleratedPaintInfo {
 #[derive(Debug, Clone, Default)]
 pub struct CefScreenInfo {
     pub device_scale_factor: f32,
-    pub depth: i32,
-    pub depth_per_component: i32,
+    pub depth: u32,
+    pub depth_per_component: u32,
     pub is_monochrome: bool,
     pub rect: CefRect,
     pub available_rect: CefRect,
 }
 
 impl CefScreenInfo {
-    pub(crate) fn from(raw: &cef_screen_info_t) -> Self {
-        CefScreenInfo {
-            device_scale_factor: raw.device_scale_factor,
-            depth: raw.depth,
-            depth_per_component: raw.depth_per_component,
-            is_monochrome: raw.is_monochrome > 0,
-            rect: CefRect::from(&raw.rect),
-            available_rect: CefRect::from(&raw.available_rect),
-        }
-    }
-
     pub(crate) fn from_mut_ptr(raw: *mut cef_screen_info_t) -> Self {
         Self::from(unsafe { &*raw })
     }
@@ -158,11 +159,24 @@ impl CefScreenInfo {
     pub fn into_raw(self) -> cef_screen_info_t {
         cef_screen_info_t {
             device_scale_factor: self.device_scale_factor,
-            depth: self.depth,
-            depth_per_component: self.depth_per_component,
+            depth: self.depth as i32,
+            depth_per_component: self.depth_per_component as i32,
             is_monochrome: self.is_monochrome.into(),
             rect: self.rect.into_raw(),
             available_rect: self.available_rect.into_raw(),
+        }
+    }
+}
+
+impl From<&cef_screen_info_t> for CefScreenInfo {
+    fn from(raw: &cef_screen_info_t) -> Self {
+        CefScreenInfo {
+            device_scale_factor: raw.device_scale_factor,
+            depth: raw.depth as u32,
+            depth_per_component: raw.depth_per_component as u32,
+            is_monochrome: raw.is_monochrome > 0,
+            rect: CefRect::from(&raw.rect),
+            available_rect: CefRect::from(&raw.available_rect),
         }
     }
 }
