9956bc284c6b3dbb83280967b86b5e193473f8f5
corentin <corentin@ltbl.fr>
2024-08-26 15:29:12 +0200

    Implement Task trait for Fn()

--- src/thread.rs
+++ src/thread.rs
@@ -6,10 +6,7 @@ pub type ThreadId = cef_thread_id_t;
 
 /// See [cef_task_t] for more documentation.
 pub trait Task: Sized {
-    fn execute(
-        &self
-    ) {
-    }
+    fn execute(&self);
 
     fn into_raw(self) -> *mut cef_task_t {
         let mut object: cef_task_t = unsafe { std::mem::zeroed() };
@@ -20,22 +17,24 @@ pub trait Task: Sized {
     }
 }
 
-
 /// See [cef_currently_on] for more documentation.
-pub fn currently_on(thread_id: ThreadId) -> bool{
+pub fn currently_on(thread_id: ThreadId) -> bool {
     unsafe { cef_currently_on(thread_id) > 0 }
 }
 
 /// See [cef_post_task] for more documentation.
-pub fn post_task(thread_id: ThreadId, task: impl Task) -> bool{
-    unsafe { cef_post_task(thread_id, task.into_raw()) > 0 } 
+pub fn post_task(thread_id: ThreadId, task: impl Task) -> bool {
+    unsafe { cef_post_task(thread_id, task.into_raw()) > 0 }
 }
 
-extern "C" fn execute<T: Task>(
-    this: *mut cef_task_t
-) {
+extern "C" fn execute<T: Task>(this: *mut cef_task_t) {
     let obj: &mut RcImpl<_, T> = RcImpl::get(this);
 
-    obj.interface
-        .execute();
+    obj.interface.execute();
+}
+
+impl<F: Fn()> Task for F {
+    fn execute(&self) {
+        self()
+    }
 }
