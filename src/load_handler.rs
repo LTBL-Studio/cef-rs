98728682459fe69dea064e71e94c6871e3be443a
corentin <corentin@ltbl.fr>
2024-09-05 17:01:58 +0200

    Add documentation to LoadHandler callbacks

--- src/load_handler.rs
+++ src/load_handler.rs
@@ -8,6 +8,12 @@ use crate::{frame::Frame, rc::RcImpl, string::CefString, Browser, ErrorCode, Tra
 /// See [cef_load_handler_t] for more documentation.
 pub trait LoadHandler: Sized {
     //get_accessibility_handler
+
+    /// Called when the loading state has changed.
+    /// 
+    /// This callback will be executed twice -- once when loading is initiated either programmatically or by user action, and once when loading is terminated due to completion, cancellation of failure.
+    /// 
+    /// It will be called before any calls to on_load_start and after all calls to on_load_error and/or on_load_end.
     fn on_loading_state_change(
         &self,
         browser: &Browser,
@@ -15,13 +21,41 @@ pub trait LoadHandler: Sized {
         can_go_back: bool,
         can_go_forward: bool,
     );
+
+    /// Called after a navigation has been committed and before the browser begins loading contents in the frame.
+    ///
+    /// Call the `is_main()` function to check if this frame is the main frame.
+    /// 
+    /// `transition_type` provides information about the source of the navigation and an accurate value is only available in the browser process.
+    /// 
+    /// Multiple frames may be loading at the same time. Sub-frames may start or continue loading after the main frame load has ended.
+    /// This function will not be called for same page navigations (fragments, history state, etc.) or for navigations that fail or are canceled before commit.
+    /// 
+    /// For notification of overall browser load status use on_loading_state_change instead.
     fn on_load_start(
         &self,
         browser: &Browser,
         frame: &mut Frame,
         transition_type: TransitionType,
     );
+
+    /// Called when the browser is done loading a frame.
+    /// 
+    /// Call the `is_main()` function to check if this frame is the main frame.
+    /// 
+    /// Multiple frames may be loading at the same time. Sub-frames may start or continue loading after the main frame load has ended.
+    /// This function will not be called for same page navigations (fragments, history state, etc.) or for navigations that fail or are canceled before commit.
+    /// 
+    /// For notification of overall browser load status use on_loading_state_change instead.
     fn on_load_end(&self, browser: &Browser, frame: &mut Frame, http_status_code: i32);
+
+    /// Called when a navigation fails or is canceled.
+    /// 
+    /// This function may be called by itself if before commit or in combination with on_load_start/on_load_end if after commit.
+    /// 
+    /// - `errorCode` is the error code number
+    /// - `errorText` is the error text
+    /// - `failedUrl` is the URL that failed to load
     fn on_load_error(
         &self,
         browser: &Browser,
